ac172041b97ab16f43f46cef54bb8ddc9b3f65ff
waterisgezond <71389925+waterisgezond@users.noreply.github.com>
2022-01-27 20:10:48 +0100

    Add files via upload

---
+++ WaterX v1/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace WaterX_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.visualStudioTabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.AddTabButton = new System.Windows.Forms.Button();
+            this.RemoveTabButton = new System.Windows.Forms.Button();
+            this.visualStudioTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(-8, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(616, 5);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(447, 60);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(148, 199);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(12, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "execute";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(99, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 32);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(186, 265);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 32);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "openfile";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(447, 265);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 32);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "inject";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(368, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 25);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "-";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(487, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 25);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(182, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "waterX";
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button8.Location = new System.Drawing.Point(273, 265);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(81, 32);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "script hub";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // visualStudioTabControl1
+            // 
+            this.visualStudioTabControl1.Controls.Add(this.tabPage1);
+            this.visualStudioTabControl1.Location = new System.Drawing.Point(12, 38);
+            this.visualStudioTabControl1.Name = "visualStudioTabControl1";
+            this.visualStudioTabControl1.SelectedIndex = 0;
+            this.visualStudioTabControl1.Size = new System.Drawing.Size(429, 221);
+            this.visualStudioTabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Black;
+            this.tabPage1.Controls.Add(this.fastColoredTextBox1);
+            this.tabPage1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(421, 195);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "new tab";
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;=]+);\r\n^\\s*(case|default)\\s*[^:]*" +
+    "(?<range>:)\\s*(?<range>[^;]+);";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(275, 14);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BackColor = System.Drawing.Color.Black;
+            this.fastColoredTextBox1.BookmarkColor = System.Drawing.Color.Black;
+            this.fastColoredTextBox1.CharHeight = 14;
+            this.fastColoredTextBox1.CharWidth = 8;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.fastColoredTextBox1.IndentBackColor = System.Drawing.Color.Black;
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.LineNumberColor = System.Drawing.Color.Lime;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(-4, 0);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.ServiceLinesColor = System.Drawing.Color.Black;
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(429, 199);
+            this.fastColoredTextBox1.TabIndex = 0;
+            this.fastColoredTextBox1.Text = "--made by a guy who loves water";
+            this.fastColoredTextBox1.Zoom = 100;
+            this.fastColoredTextBox1.Load += new System.EventHandler(this.fastColoredTextBox1_Load_1);
+            // 
+            // AddTabButton
+            // 
+            this.AddTabButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddTabButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddTabButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.AddTabButton.Location = new System.Drawing.Point(65, 38);
+            this.AddTabButton.Name = "AddTabButton";
+            this.AddTabButton.Size = new System.Drawing.Size(28, 22);
+            this.AddTabButton.TabIndex = 14;
+            this.AddTabButton.Text = "+";
+            this.AddTabButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.AddTabButton.UseVisualStyleBackColor = true;
+            this.AddTabButton.Click += new System.EventHandler(this.AddTabButton_Click);
+            // 
+            // RemoveTabButton
+            // 
+            this.RemoveTabButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RemoveTabButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemoveTabButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.RemoveTabButton.Location = new System.Drawing.Point(92, 38);
+            this.RemoveTabButton.Name = "RemoveTabButton";
+            this.RemoveTabButton.Size = new System.Drawing.Size(27, 22);
+            this.RemoveTabButton.TabIndex = 15;
+            this.RemoveTabButton.Text = "x";
+            this.RemoveTabButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.RemoveTabButton.UseVisualStyleBackColor = true;
+            this.RemoveTabButton.Click += new System.EventHandler(this.RemoveTabButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(603, 307);
+            this.Controls.Add(this.RemoveTabButton);
+            this.Controls.Add(this.AddTabButton);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.visualStudioTabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "execute";
+            this.visualStudioTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TabControl visualStudioTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private System.Windows.Forms.Button AddTabButton;
+        private System.Windows.Forms.Button RemoveTabButton;
+    }
+}
+
---
+++ WaterX v1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WeAreDevs_API;
+using System.Drawing.Drawing2D;
+
+namespace WaterX_v1
+{
+    public partial class Form1 : Form
+    {
+        WeAreDevs_API.ExploitAPI api = new WeAreDevs_API.ExploitAPI();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            api.SendLimitedLuaScript(fastColoredTextBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            api.LaunchExploit();
+            if(api.isAPIAttached())
+            {
+                button4.Text = "injected";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opendialogfile = new OpenFileDialog();
+            opendialogfile.Filter = "Lua File (*.lua)|*.lua|Text File (*.txt)|*.txt";
+            opendialogfile.FilterIndex = 2;
+            opendialogfile.RestoreDirectory = true;
+            if (opendialogfile.ShowDialog() != DialogResult.OK)
+                return;
+            try
+            {
+                fastColoredTextBox1.Text = "";
+                System.IO.Stream stream;
+                if ((stream = opendialogfile.OpenFile()) == null)
+                    return;
+                using (stream)
+                    this.fastColoredTextBox1.Text = System.IO.File.ReadAllText(opendialogfile.FileName);
+            }
+            catch (Exception ex)
+            {
+                int num = (int)MessageBox.Show("can´t attach", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void fastColoredTextBox1_Load(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.Lua;
+        }
+
+        private void AddTabButton_Click(object sender, EventArgs e)
+        {
+            TabPage newTab = new TabPage();
+            FastColoredTextBoxNS.FastColoredTextBox textBox = new FastColoredTextBoxNS.FastColoredTextBox();
+            newTab.Name = "Script" + (visualStudioTabControl1.TabCount + 1);
+            newTab.Text = "New Tab  ";
+            newTab.Parent = visualStudioTabControl1;
+            textBox.Dock = DockStyle.Fill;
+            textBox.Name = "fastColoredTextBox1";
+            textBox.Parent = newTab;
+            visualStudioTabControl1.SelectTab(newTab);
+            AddTabButton.Left = AddTabButton.Left + 75;
+            RemoveTabButton.Left = RemoveTabButton.Left + 77;
+            if (visualStudioTabControl1.TabCount == 7)
+            {
+                AddTabButton.Hide();
+            }
+            if (visualStudioTabControl1.TabCount > 1)
+            {
+                RemoveTabButton.Show();
+            }
+        }
+
+        private void RemoveTabButton_Click(object sender, EventArgs e)
+        {
+            if (visualStudioTabControl1.TabCount > 1)
+            {
+                Control tabPageToRemove = visualStudioTabControl1.Controls["Script" + (visualStudioTabControl1.TabCount)];
+                visualStudioTabControl1.SelectTab("Script" + (visualStudioTabControl1.TabCount - 1));
+                visualStudioTabControl1.Controls.Remove(tabPageToRemove);
+                AddTabButton.Left = AddTabButton.Left - 75;
+                RemoveTabButton.Left = RemoveTabButton.Left - 77;
+                if (visualStudioTabControl1.TabCount == 7)
+                {
+                    AddTabButton.Hide();
+                }
+                else
+                {
+                    AddTabButton.Show();
+                }
+                if (visualStudioTabControl1.TabCount == 1)
+                {
+                    RemoveTabButton.Hide();
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        public void fastColoredTextBox1_Load_1(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.Lua;
+        }
+    }
+}
